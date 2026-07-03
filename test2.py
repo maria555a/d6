37bb41134ab12e3f6db7f79eefc9e0abed5a35c4
Maria <theweirdrider@gmail.com>
2026-07-03 23:50:57 +0300

    just for fun

--- test2.py
+++ test2.py
@@ -1,1 +1,8 @@
+w1 = "Alexey"
+w2 = "Suvorov"
+w3 = "Is 26 years old"
+phrase = w1 + " " + w2 + " " + w3
+print(phrase)
+
+
 
---
+++ test3.py
@@ -0,0 +1,13 @@
+while True:
+   secret_code = int(input("Enter code: "))
+   secret_name = "Heil Satan!"
+
+   if secret_code not in {666}:
+       print('Wrong value')
+       continue
+
+   if secret_code == "666":
+     result = secret_name
+   print('Heil Satan!')
+   break
+
